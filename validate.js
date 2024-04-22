c0fa864662540c4fd6606c8896650b035611e6f2
ValdemirVieira <brenoviera.s@gmail.com>
2024-04-22 16:12:50 -0300

    Implementando JS

--- validate.js
+++ validate.js
@@ -1,1 +1,9 @@
 //Seu JavaScript de validação aqui
+const inputNome = document.getElementById('nome');
+const inputEmail = document.getElementById('email');
+const inputAssunto = document.getElementById('assunto');
+const inputMensagem = document.getElementById('mensagem');
+
+function btnValidar() {
+    const validar = document.querySelector('form').value;
+}
