0347f9b2ff68ff2c9a21a269bb5c0dfffca024d1
ValdemirVieira <brenoviera.s@gmail.com>
2024-04-23 16:21:59 -0300

    Iniciando as funções de validação.

--- validate.js
+++ validate.js
@@ -3,7 +3,16 @@ const inputNome = document.getElementById('nome');
 const inputEmail = document.getElementById('email');
 const inputAssunto = document.getElementById('assunto');
 const inputMensagem = document.getElementById('mensagem');
+const botao = document.getElementById('botao');
+const mensagemErro = document.getElementById('msgErr');
 
 function btnValidar() {
     const validar = document.querySelector('form').value;
 }
+
+function validarNome() {}
+function validarEmail() {}
+function validarAssunto() {}
+function validarMensagem() {}
+function validarform() {}
+function validarMsgErr() {}
